736c98fa25aed4efb8ad67dfef9fc873d85ad22e
Toshan Kumar <toshan.dc@gmail.com>
2025-09-21 08:49:55 -0700

    Update plurality.c

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -4,36 +4,29 @@
 #include <stdio.h>
 #include <string.h>
 
-// Max number of candidates
 #define MAX 9
 
-// Candidates have name and vote count
 typedef struct
 {
     string name;
     int votes;
 } candidate;
 
-// Array of candidates
 candidate candidates[MAX];
 
-// Number of candidates
 int candidate_count;
 
-// Function prototypes
 bool vote(string name);
 void print_winner(void);
 
 int main(int argc, string argv[])
 {
-    // Check for invalid usage
     if (argc < 2)
     {
         printf("Usage: plurality [candidate ...]\n");
         return 1;
     }
 
-    // Populate array of candidates
     candidate_count = argc - 1;
     if (candidate_count > MAX)
     {
@@ -48,23 +41,19 @@ int main(int argc, string argv[])
 
     int voter_count = get_int("Number of voters: ");
 
-    // Loop over all voters
     for (int i = 0; i < voter_count; i++)
     {
         string name = get_string("Vote: ");
 
-        // Check for invalid vote
         if (!vote(name))
         {
             printf("Invalid vote.\n");
         }
     }
 
-    // Display winner of election
     print_winner();
 }
 
-// Update vote totals given a new vote
 bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
@@ -78,7 +67,6 @@ bool vote(string name)
     return false;
 }
 
-// Print the winner (or winners) of the election
 void print_winner(void)
 {
     int max_votes = 0;
